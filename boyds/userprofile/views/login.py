2a6e7602872d85a0f56a3fa8b9471bf1b99f159d
justin <justin.mearns@gmail.com>
2016-09-16 14:30:21 +0800

    moved user related files to userprofile app

--- boyds/userprofile/views/login.py
+++ boyds/userprofile/views/login.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-from inventory.forms import UserForm
+from userprofile.forms import UserForm
 
 
 class LoginView(TemplateView):
