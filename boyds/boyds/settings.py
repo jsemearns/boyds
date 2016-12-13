db12d221d10fdd96b57571785e501c755c02eafd
justin <justin.mearns@gmail.com>
2016-12-13 12:21:40 +0800

    added loginrequiredmixin to dashboard view; upgrade django version to 1.9

--- boyds/boyds/settings.py
+++ boyds/boyds/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.staticfiles',
     'inventory',
     'userprofile',
+    'dashboard',
 )
 
 MIDDLEWARE_CLASSES = (
--- boyds/dashboard/views.py
+++ boyds/dashboard/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.urlresolvers import reverse
 
 
-class DashboardView(TemplateView):
-	template_name = ''
-	def get(self, request, *args, **kwargs):
-		return HttpResponse()
+class DashboardView(LoginRequiredMixin, TemplateView):
+    template_name = 'dashboard.html'
+    login_url = '/login/'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return HttpResponse(render(request, self.template_name, context))
